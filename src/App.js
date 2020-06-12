9199cc79a9979a067d0e8821d624067f7e18aa09
raviteja8k <raviteja.tk@gmail.com>
2020-06-12 16:53:01 +0530

    Using ComponentDidMount

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 class App extends Component {
   constructor (props){
     super(props);
-    this.state={title: ''};
+    this.state={title: '',
+                userData: ''};
     this.setTitle = this.setTitle.bind(this)
   }
 
@@ -19,12 +20,38 @@ class App extends Component {
   }
 
   render(){
+    let users = null;
+    if(this.state.userData){
+      users =  
+      //this.state.userData;
+    this.state.userData.map((user, i) => <div key={i}>{user.name}  -{user.address.city}</div>) 
+    }
+     else {
+      users = <p>Loading...</p>;
+    } 
+
   return (
     <div className="App">
       <h1>{this.state.title}</h1>
+      {users}
     </div>
   );
   }
+ 
+  // Re-renders the component with data fetched from dummy API
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res) => res.json())
+    .then((json) => 
+       setTimeout(() => {
+        this.setState({userData: json})
+        }, 1000)          
+    ) 
+
+        
+  }
+    
+
 }
 
 export default App;
